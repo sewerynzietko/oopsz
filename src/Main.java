1acceb967739924e59a436a30611a787edf2e422
sewerynzietko <sewezietko@gmail.com>
2025-03-10 14:23:17 +0100

    lab2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p = new Point();
+        p.x = 30; p.y = 50;
+        System.out.println(p.toSvg());
+
+    }
+}
+
+//klkkk
